c03d1d1e7ed08557aa1df3fa5b147a0ae35a97eb
Filippakis George <g.filippakis@intralot.com>
2018-03-29 11:09:45 +0300

    Refactored MakeActionCreator

--- src/utility/makeActionCreator.js
+++ src/utility/makeActionCreator.js
@@ -1,8 +1,14 @@
-// TODO: Use map to make it more terse
-export const makeActionCreator = (type, ...actionPropertiesNames) => (...actionPropertiesValues) => {
-    let action = { type };
-    actionPropertiesNames.forEach((arg, index) => {
-        action[actionPropertiesNames[index]] = actionPropertiesValues[index];
-    });
-    return action;
-};
+export const makeActionCreator = (type, ...actionPropertiesNames) => (...actionPropertiesValues) =>
+    actionPropertiesNames.reduce((action, item, index) => {
+        action[item] = actionPropertiesValues[index];
+        return action;
+    }, { type });
+
+// export const makeActionCreator = (type, ...actionPropertiesNames) => (...actionPropertiesValues) => {
+//     let action = { type };
+//     actionPropertiesNames.forEach((arg, index) => {
+//         action[actionPropertiesNames[index]] = actionPropertiesValues[index];
+//     });
+//     console.log('I created this action with forEach: ', action);
+//     return action;
+// };
