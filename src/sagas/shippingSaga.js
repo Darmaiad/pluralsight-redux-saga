c6463b010a80b6a65bad151734467afa2d1455c5
Filippakis George <g.filippakis@intralot.com>
2018-04-02 14:13:10 +0300

    Shipping Saga

---
+++ src/sagas/shippingSaga.js
@@ -0,0 +1,38 @@
+import { select, put, takeLatest } from 'redux-saga/effects';
+import fetch from 'isomorphic-fetch';
+
+import {
+    SET_CART_ITEMS,
+    INCREASE_ITEM_QUANTITY,
+    DECREASE_ITEM_QUANTITY,
+    FETCHED,
+    FETCHING,
+    setShippingFetchStatus,
+    setShippingCost,
+} from './../actions';
+import { cartItemsSelector } from './../selectors/cartItemsSelector';
+
+function* shipping() {
+    yield put(setShippingFetchStatus(FETCHING));
+    const items = yield select(cartItemsSelector);
+
+    // For each item 
+    const itemRequestString = items.reduce((string, item) => {
+        // For that many times as its quantity
+        for (let i = 0; i < item.get('quantity'); i++) {
+            // Add its Id and a comma to the string
+            string += item.get('id') + ',';
+        }
+        return string;
+    }, "").replace(/,\s*$/, '');
+
+    const response = yield fetch(`http://localhost:8081/shipping/${itemRequestString}`);
+    const { total } = yield response.json();
+
+    yield put(setShippingCost(total));
+    yield put(setShippingFetchStatus(FETCHED));
+}
+
+export function* shippingSaga() {
+    yield takeLatest([SET_CART_ITEMS, INCREASE_ITEM_QUANTITY, DECREASE_ITEM_QUANTITY], shipping);
+}
