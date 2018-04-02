e7cf4221298e3e4f228053e2c7dc0acf735493d8
Filippakis George <g.filippakis@intralot.com>
2018-04-02 14:57:35 +0300

    Tax rate Saga

---
+++ src/sagas/taxRateSaga.js
@@ -0,0 +1,15 @@
+import { take, put } from 'redux-saga/effects';
+import fetch from 'isomorphic-fetch';
+
+import { SET_CURRENT_USER, setTaxRate } from './../actions';
+
+export function* taxRateSaga() {
+    // This line will run in the beginning of the App
+    const { user } = yield take(SET_CURRENT_USER);
+    // Those lines will run when the SET_CURRENT_USER action is sent
+    const { country } = user;
+    const response = yield fetch(`http://localhost:8081/tax/${country}`);
+    const { rate } = yield response.json();
+
+    yield put(setTaxRate(rate));
+}
