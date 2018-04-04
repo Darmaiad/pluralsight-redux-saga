f90357c19dbd6dc9609dc4722842667debbddcfa
Filippakis George <g.filippakis@intralot.com>
2018-04-04 15:29:57 +0300

    Dependency cleanup

--- server/buildHTML.js
+++ server/buildHTML.js
@@ -3,7 +3,6 @@
 // In this case, the transformation is useful since we only use a separate css file in prod.
 import fs from 'fs';
 import cheerio from 'cheerio';
-import colors from 'colors';
 
 /* eslint-disable no-console */
 
