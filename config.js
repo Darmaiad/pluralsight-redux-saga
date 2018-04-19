742426941911e0f68bc397bb9945d94dc5e028f8
Filippakis George <g.filippakis@intralot.com>
2018-04-19 15:39:12 +0300

    Changed config module to node from es6

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
+require('dotenv').config();
 
 const ENV = process.env.NODE_ENV.toUpperCase();
 
-export default {
+module.exports = {
     env: process.env.NODE_ENV,
     port: parseInt(process.env[`${ENV}_PORT`], 10) || 9000,
     host: process.env[`${ENV}_HOST`] || 'localhost',
